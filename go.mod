84e1a6fc1a811dbf7c175b7ea201125d6b1dcf14
Shweta Moharil <shweta.moharil@gslab.com>
2022-06-06 18:03:21 +0530

    Init commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module URL-Shortner
+
+go 1.17
+
+require github.com/gorilla/mux v1.8.0 // indirect
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+	http.ListenAndServe(":8000", router)
+}
